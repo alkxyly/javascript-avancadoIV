a71d7039ff31dd28b1ade347ba7da92889ef9893
Alkxyly <alkxyly@hotmail.com>
2020-03-10 15:16:31 -0300

    Clicando no máx 3 vezes uma chamada ao service

--- public/app/app.js
+++ public/app/app.js
@@ -1,18 +1,26 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 
-document
-    .querySelector('#myButton')
-    .onclick = () =>
+
+
+const operation = takeUntil(3, () =>
     service
     .sumItems('2143')
     .then(console.log)
-    .catch(console.log);
+    .catch(console.log)
+);
+
+
+
+document
+    .querySelector('#myButton')
+    .onclick = () => operation();
 
 
 
 const ehDivisivel = (divisor, numero) => !(numero % divisor);
 
 const ehDivisivelPorDois = ehDivisivel.bind(null, 2);
-console.log(ehDivisivelPorDois(15));
+//console.log(ehDivisivelPorDois(15));
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -5,4 +5,10 @@ export const partialize = (fn, ...args) =>
 export const compose = (...fns) => value =>
     fns.reduceRight((previousValue, fn) => fn(previousValue), value)
 
-//const sumItems = compose(sumItems, filterItems, getItemsFromNotas);
+
+
+export const pipe = (...fns) => value =>
+    fns.reduce((previousValue, fn) => fn(previousValue), value)
+
+export const takeUntil = (times, fn) =>
+    () => (times-- > 0 && fn());

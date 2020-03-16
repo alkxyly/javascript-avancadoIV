4debe105c3ab1e271ff0fac7576297fb087bd8d4
Alkxyly <alkxyly@hotmail.com>
2020-03-16 11:31:02 -0300

    Composição de função

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,25 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-const action = debounceTime(500,
-    takeUntil(3, () =>
-        service
-        .sumItems('2143')
-        .then(console.log)
-        .catch(console.log)
-    ));
+
+
+const partializedTakeUntil = partialize(takeUntil, 3);
+
+const doTake = partializedTakeUntil(() => console.log('oi'));
+
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+    service
+    .sumItems('2143')
+    .then(console.log)
+    .catch(console.log)
+)
 
 document
     .querySelector('#myButton')
